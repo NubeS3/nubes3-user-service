cc5e2968dd854d6a146f250b797dfde78f6c88fb
Pham Tuan <phamtuan199911@gmail.com>
2021-04-11 12:07:31 +0700

    Switch to use normal clean up

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,6 @@ import (
 	message_queue "github.com/Nubes3/nubes3-user-service/internal/api/message-queue"
 	restApi "github.com/Nubes3/nubes3-user-service/internal/api/rest-api"
 	"github.com/Nubes3/nubes3-user-service/internal/repo/arangodb"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
@@ -23,18 +20,10 @@ func main() {
 
 	arangodb.InitArangoRepo()
 
-	sigs := make(chan os.Signal, 1)
-	cleanupDone := make(chan bool)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-sigs
-		for _, f := range cleanUps {
-			f()
-		}
-		cleanupDone <- true
-	}()
+	for _, cleanUpF := range cleanUps {
+		defer cleanUpF()
+	}
 
 	fmt.Println("User service's running")
 	restApi.Serve()
-	<-cleanupDone
 }
